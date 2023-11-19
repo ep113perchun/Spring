7542b881a1b8322bdfc41f7cb955ba60f9034796
Vladislav <vlad.perchun.93@gmail.com>
2023-11-19 13:24:07 +0700

    Dependency Ingection (DI) + technical task

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,36 +1,34 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
     private Music music;
+    private List<Music> musicList = new ArrayList<>();
     private String name;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
     private int volume;
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getVolume() {
         return volume;
     }
-
     public void setVolume(int volume) {
         this.volume = volume;
     }
-
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-    public MusicPlayer(){}
-
     public void setMusic(Music music) {
         this.music = music;
     }
-
     public void playMusic(){
-        System.out.println("Playing: " + music.getSong());
+        for (Music a : musicList) {
+            System.out.println("Playing: " + a.getSong());
+        }
     }
 }
---
+++ src/main/java/org/example/RepMusic.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class RepMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Eminem";
+    }
+}
